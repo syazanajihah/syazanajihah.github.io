39ffc7bd65c7ca6b98b9aa0b97f1bb9f7f043170
Nur Zahirah <44749339+zahirahzamri@users.noreply.github.com>
2018-12-16 12:37:53 +0800

    Update pizza.js

--- pizzaz/pizza.js
+++ pizzaz/pizza.js
@@ -1,59 +1,97 @@
-$("#orderthis1").click(function(){
-	var order = $(this).val();
-	var add;
+// var addpizza = ["Pepperoni", "Meat-mania", "Meatasaurus", "Extravaganza", "Vegie-Galore", "Baked-Perfection"]
+//array that hold all ordered products
+var listPizzaAdd = [];
 
-	if(order == 'Pepperoni'){
-		add += order;
-		$('#showcart').html('Pepperoni');
-	}
-})
+function displayShoppingCart(){
+		var orderedProducts = document.getElementById('orderedProductsTblBody');
 
-$("#orderthis2").click(function(){
-	var order = $(this).val();
-	var add;
+		while(orderedProducts.rows.length > 0){
+			orderedProducts.deleteRow(0);
+		}
 
-	if (order == 'Meat-mania') {
-		add += order;
-		$('#showcart').html('Meat-mania');
-	}
-})
+	//variable that hold total price
+		var cart_total_price = 0;
 
-$("#orderthis3").click(function(){
-	var order = $(this).val();
-	var add;
+		for(var product in listPizzaAdd){
+			var row = orderedProducts.insertRow();
 
-	if (order == 'Meatasaurus') {
-		add += order;
-		$('#showcart').html('Meatasaurus');
-	}
-})
+			var cellName = row.insertCell(0);
+			var cellDescription = row.insertCell(1);
+			var cellPrice = row.insertCell(2);
+			cellPrice.align="right";
+		 //fill cells with values from current product object of our array
+			cellName.innerHTML = listPizzaAdd[product].Name;
+			cellDescription.innerHTML = listPizzaAdd[product].Description;
+			cellPrice.innerHTML = listPizzaAdd[product].Price;
+			cart_total_price+=listPizzaAdd[product].Price;
+		}
 
-$("#orderthis4").click(function(){
-	var order = $(this).val();
-	var add;
+			document.getElementById("showcart").innerHTML=cart_total_price;
+}
 
-	if (order == 'Extravaganza') {
-		add += order;
-		$('#showcart').html('Extravaganza');
-	}
-})
+function AddtoCart(name,description,price){
+       //Below we create JavaScript Object that will hold three properties you have mentioned:    Name,Description and Price
+       var singleProduct = {};
+       //Fill the product object with data
+       singleProduct.Name=name;
+       singleProduct.Description=description;
+       singleProduct.Price=price;
+       //Add newly created product to our shopping cart
+       listPizzaAdd.push(singleProduct);
+       //call display function to show on screen
+       displayShoppingCart();
+}
 
-$("#orderthis5").click(function(){
-	var order = $(this).val();
-	var add;
-
-	if (order == 'Vegie-Galore') {
-		add += order;
-		$('#showcart').html('Vegie-Galore');
-	}
-})
-
-$("#orderthis6").click(function(){
-	var order = $(this).val();
-	var add;
-
-	if (order == 'Baked-Perfection') {
-		add += order;
-		$('#showcart').html('Baked-Perfection');
-	}
-})
+//
+// $("#orderthis1").click(function(){
+// 	var order = $(this).val();
+// 	var first = addpizza[0];
+//
+// 	if(order == 'Pepperoni'){
+// 		listPizzaAdd.push(first);
+// 		// $('#showcart').html('Pepperoni');
+// 		listPizza(listPizzaAdd);
+// 	}
+// })
+// $("#orderthis2").click(function(){
+// 	var order = $(this).val();
+// 	var second = addpizza[1];
+//
+// 	if (order == 'Meat-mania') {
+// 		listPizzaAdd.push(second);
+// 		// $('#showcart').html('Meat-mania');
+// 		listPizza(listPizzaAdd);
+// 	}
+// })
+// $("#orderthis3").click(function(){
+// 	var order = $(this).val();
+// 	var third = addpizza[2];
+//
+// 	if (order == 'Meatasaurus') {
+// 		$('#showcart').html('Meatasaurus');
+// 	}
+// })
+// $("#orderthis4").click(function(){
+// 	var order = $(this).val();
+// 	var fourth = addpizza[3];
+//
+// 	if (order == 'Extravaganza') {
+// 		$('#showcart').html('Extravaganza');
+// 	}
+// })
+// $("#orderthis5").click(function(){
+// 	var order = $(this).val();
+// 	var fifth = addpizza[4];
+//
+// 	if (order == 'Vegie-Galore') {
+// 		$('#showcart').html('Vegie-Galore');
+// 	}
+// })
+// $("#orderthis6").click(function(){
+// 	var order = $(this).val();
+// 	var sixth = addpizza[5];
+//
+// 	if (order == 'Baked-Perfection') {
+// 		$('#showcart').html(sixth);
+// 	}
+// })
